e694a1149e11f221c94636c5647aa9d99862faa9
ivonzhang <zyfdtxz@hotmail.com>
2020-07-03 00:54:42 +0200

    Upload dir based on filename

--- lib/loading_page.dart
+++ lib/loading_page.dart
@@ -46,7 +46,11 @@ class _LoadingPageState extends State<LoadingPage>
         }
         try {
           int patientNr = int.parse(filename.split(' ').first);
-          final StorageReference ref = FirebaseStorage().ref().child('/Single $patientNr/$filename');
+          bool isDual = filename.split('_').last.startsWith("dual");
+          final StorageReference ref = FirebaseStorage().ref().child(
+            isDual
+              ? '/Dual $patientNr/$filename'
+              : '/Single $patientNr/$filename');
           var uploadTask = ref.putFile(f);
           StorageTaskSnapshot taskSnapshot = await uploadTask.onComplete;
 
